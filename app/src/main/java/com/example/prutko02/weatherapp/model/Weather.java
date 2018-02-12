de6297710d160ff25172a51f20373614a551e980
Paweł Rutkowski <P.Rutkowski@hypermedia.pl>
2018-02-12 02:53:01 +0100

    Add data binding

---
+++ app/src/main/java/com/example/prutko02/weatherapp/model/Weather.java
@@ -0,0 +1,9 @@
+package com.example.prutko02.weatherapp.model;
+
+public class Weather {
+    private Double temperature;
+
+    public Double getTemperature() {
+        return temperature;
+    }
+}
--- app/src/main/java/com/example/prutko02/weatherapp/view/WeatherActivity.java
+++ app/src/main/java/com/example/prutko02/weatherapp/view/WeatherActivity.java
@@ -1,15 +1,20 @@
 package com.example.prutko02.weatherapp.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.prutko02.weatherapp.R;
+import com.example.prutko02.weatherapp.databinding.WeatherActivityBinding;
+import com.example.prutko02.weatherapp.model.Weather;
 
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        WeatherActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.weather_activity);
+        Weather weather = new Weather();
+        binding.setWeather(weather);
     }
 }
